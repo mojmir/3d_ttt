ae4445898e5159a53b15c348da0f5aec999e2c2f
Jozef Genzor <jozef.genzor@gmail.com>
2016-03-24 17:07:38 +0100

    Update field.py
    added check_winner method, last_move member variable (updated in add_stone method)

--- interface/field.py
+++ interface/field.py
@@ -11,10 +11,10 @@ class Field(object):
         solved: there is already winner
 
     Methods:
-        add_stone: add point of selected value if possible in matrix
+        add_stone: add stone of selected value if possible in matrix
         create matrix: return 3D list filled with zero value points, plain field
         guess_point:
-        check_winner:
+        check_winner: check if there is a winner after the last move
         display:
     """
 
@@ -23,6 +23,7 @@ class Field(object):
         self.dimension_y = dimension_y
         self.dimension_z = dimension_z
         self.matrix = self.create_matrix(dimension_x, dimension_y, dimension_z)
+        self.last_move = None
 
     def create_matrix(self, width, length, height):
         """
@@ -51,6 +52,62 @@ class Field(object):
                 raise ValueError("Filling already taken point")
         else:
             raise ValueError("Adding point outside of field boundaries")
+        self.last_move = (i, j, k)
+
+    def check_winner(self, n): #n is the number of stones in line required to win
+        """
+        Check if there is a winner after the last move
+        """
+        dimensions = (self.dimension_x, self.dimension_y, self.dimension_z)
+        x, y, z = self.last_move
+        memory = (self.matrix[x][y][z]).value
+        directions = ( (1,0,0),
+                       (0,1,0),
+                       (0,0,1),
+                       (1,1,0),
+                       (1,-1,0),
+                       (1,0,1),
+                       (1,0,-1),
+                       (0,1,1),
+                       (0,1,-1),
+                       (1,1,1),
+                       (1,1,-1),
+                       (1,-1,1),
+                       (-1,1,1) )
+        for aDirection in directions:
+            position = list(self.last_move)
+            count = 0
+            value = memory
+            while ( memory == value ):
+                position = [position[i] + aDirection[i] for i in range(3)]
+                count += 1
+                if count == n:
+                    return memory #return the winner
+                flag = False
+                for i in range(3):
+                    if (position[i] == dimensions[i]) or (position[i] == -1):
+                        flag = True
+                if flag:
+                    break
+                i, j, k = position
+                value = (self.matrix[i][j][k]).value
+            position = list(self.last_move)
+            value = memory
+            count -= 1
+            while ( memory == value ):
+                position = [position[i] - aDirection[i] for i in range(3)]
+                count += 1
+                if count == n:
+                    return memory #return the winner
+                flag = False
+                for i in range(3):
+                    if (position[i] == dimensions[i]) or (position[i] == -1):
+                        flag = True
+                if flag:
+                    break
+                i, j, k = position
+                value = (self.matrix[i][j][k]).value
+        return 0 #if no winner was found
 
     def __str__(self):
         """
